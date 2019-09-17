5cf83b950aac3b077ca46a913d513b6ea9419c2c
Ashishfc <ashishs@freightcrate.in>
2019-09-17 14:13:20 +0530

    mixin-custome-response </> Tue, 2019-09-17, 14:13:20 IST 260

--- projdjvue/student/views.py
+++ projdjvue/student/views.py
@@ -7,14 +7,12 @@ from rest_framework.response import Response
 from django.core import serializers
 from django.http import HttpResponse
 from django.http import JsonResponse
-
-# Create your views here.
+from rest_framework import status
 
 
 class StudentDetailsList(mixins.ListModelMixin,
-                          mixins.CreateModelMixin,
-                          generics.GenericAPIView):
-
+                         mixins.CreateModelMixin,
+                         generics.GenericAPIView):
     queryset = StudentDetails.objects.all()
     serializer_class = StudentDetailsSerializer
     lookup_field = 'pk'
@@ -22,15 +20,25 @@ class StudentDetailsList(mixins.ListModelMixin,
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset().order_by('-id')
+        serializer = StudentDetailsSerializer(queryset, many=True)
+        return Response({'data': serializer.data})
+
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
+    def create(self, request, *args, **kwargs):
+        serializer = StudentDetailsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'data': serializer.data})
+
 
 class StudentDetailsDetail(mixins.RetrieveModelMixin,
-                            mixins.UpdateModelMixin,
-                            mixins.DestroyModelMixin,
-                            generics.GenericAPIView):
-
+                           mixins.UpdateModelMixin,
+                           mixins.DestroyModelMixin,
+                           generics.GenericAPIView):
     queryset = StudentDetails.objects.all()
     serializer_class = StudentDetailsSerializer
 
@@ -45,5 +53,20 @@ class StudentDetailsDetail(mixins.RetrieveModelMixin,
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
 
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = StudentDetailsSerializer(
+            instance=instance,
+            data=request.data
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'data': serializer.data})
+
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({'data': '{}'}, status=status.HTTP_204_NO_CONTENT)
